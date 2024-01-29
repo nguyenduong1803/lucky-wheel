8e06ab9613907832207f0b1f46e0987fe9b6633b
unknown <IIST@IIST-2019-PC028.iist1.local>
2024-01-29 13:17:35 +0700

    init-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
+import { Wheel } from "react-custom-roulette";
+import { Box, Button, Typography, Stack, Divider } from "@mui/material";
+import rotate from "./assets/audio/rotate.mp3";
+import claping from "./assets/audio/claping.mp3";
+import { data } from "./data";
 
 function App() {
+  const [mustSpin, setMustSpin] = useState(false);
+  const [prizeNumber, setPrizeNumber] = useState(0);
+  const [open, setOpen] = useState(false);
+  const remainingData = useRef(data);
+  const refRotate = useRef(null);
+  const refClaping = useRef(null);
+
+  const handleClose = () => {
+    refClaping.current.currentTime = 0;
+    refClaping.current.pause();
+    setOpen(false);
+  };
+
+  const handleSpinClick = async () => {
+    if (!mustSpin) {
+      const randomElement =
+        remainingData.current[
+          Math.floor(Math.random() * remainingData.current.length)
+        ];
+
+      const newList = remainingData.current.filter(
+        (item) => item.option !== randomElement.option
+      );
+
+      const index = data.findIndex(
+        (item) => item.option === randomElement.option
+      );
+
+      remainingData.current = newList;
+
+      setPrizeNumber(index);
+      setMustSpin(true);
+
+      await new Promise(() => {
+        setTimeout(() => {
+          refRotate.current.play();
+        }, 1500);
+      });
+    }
+  };
+
+  const styleOpen = {
+    transform: "translate(-50%,-50%) scale(1)",
+    visible: "visible",
+  };
+  const styleClose = {
+    transform: "translate(-50%,-50%) scale(0)",
+    visible: "hidden",
+  };
+
+  const computeStyle = open ? styleOpen : styleClose;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="app">
+        <div className="wrapper-weel">
+          <Wheel
+            outerBorderColor="#fff"
+            innerBorderWidth={8}
+            spinDuration={0.8}
+            innerBorderColor="#fff"
+            outerBorderWidth={8}
+            mustStartSpinning={mustSpin}
+            prizeNumber={prizeNumber}
+            data={data}
+            onStopSpinning={() => {
+              setMustSpin(false);
+              setOpen(true);
+              refRotate.current.pause();
+              refRotate.current.currentTime = 0;
+              refClaping.current.play();
+            }}
+            radiusLineColor="#fff"
+            radiusLineWidth={1}
+            textColors={["#ffffff"]}
+          />
+          <Button
+            sx={{ marginTop: "20px" }}
+            color="error"
+            size="large"
+            onClick={handleSpinClick}
+            variant="contained"
+          >
+            Quay số
+          </Button>
+        </div>
+      </div>
+      <Box
+        sx={{
+          transition: "0.6s ease",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          visibility: open ? "visible" : "hidden",
+          opacity: open ? 2 : 0,
+          top: 0,
+          right: 0,
+          left: 0,
+          bottom: 0,
+          position: "fixed",
+          zIndex: 999,
+        }}
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            transition: "0.4s ease",
+            top: "50%",
+            left: "50%",
+            backgroundColor: "background.paper",
+            borderRadius: "6px",
+            boxShadow: 24,
+            p: 2,
+            width: 1200,
+            ...computeStyle,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Typography color="error" variant="h2">
+            Câu hỏi dành cho bạn:
+          </Typography>
+          <Divider sx={{my:2}}/>
+          <Typography fontSize={40}> {data[prizeNumber].question}</Typography>
+          <Stack alignItems="end">
+            <Button onClick={handleClose}>Đóng</Button>
+          </Stack>
+        </Box>
+        <div className="pyro">
+          <div className="before"></div>
+          <div className="after"></div>
+        </div>
+      </Box>
+      <Box sx={{ visibility: "hidden", opacity: 0 }}>
+        <audio ref={refRotate} src={rotate} type="audio/ogg" />
+        <audio ref={refClaping} src={claping} type="audio/ogg" />
+      </Box>
+    </>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,73 @@
+export const data = [
+  {
+    option: "Câu hỏi số 1",
+    style: {
+      backgroundColor: "#fa2f0e",
+      textColor: "white",
+    },
+    question:
+      "Con gì mang được cả miếng gỗ lớn, nhưng không mang nổi một hòn sỏi nhỏ?",
+  },
+  {
+    option: "Câu hỏi số 2",
+    style: {
+      backgroundColor: "#ef7717",
+      textColor: "white",
+    },
+    question:
+      "Cái gì chặt không đứt, bứt không rời, phơi không khô, đốt không cháy?",
+  },
+  {
+    option: "Câu hỏi số 3",
+    style: {
+      backgroundColor: "#dd8b12",
+      textColor: "white",
+    },
+    question: "Cái gì có kích thước bằng con voi nhưng chẳng nặng gram nào cả?",
+  },
+  {
+    option: "Câu hỏi số 4",
+    style: {
+      backgroundColor: "#1366b6",
+      textColor: "white",
+    },
+    question:
+      "Giả sử ta có 1 khúc vải, cắt nó ra làm 100 khúc, thời gian để cắt 1 khúc vải là 5 giây. Hỏi nếu cắt liên tục không ngừng nghỉ thì trong bao lâu sẽ cắt xong?",
+  },
+  {
+    option: "Câu hỏi số 5",
+    style: {
+      backgroundColor: "#d80c65",
+      textColor: "white",
+    },
+    question:
+      "Bạn có 'chiêu' nào để có thể thức trắng 7 ngày mà vẫn không thiếu ngủ?",
+  },
+  {
+    option: "Câu hỏi số 6",
+    style: {
+      backgroundColor: "#520af1",
+      textColor: "white",
+    },
+    question:
+      "Có 2 con vịt đi trước 2 con vịt, lại có 2 con vịt đi sau 2 con vịt và có 2 con vịt đi giữa 2 con vịt. Hỏi có mấy con vịt Ở Châu Phi, chim cánh cụt sống ở nước nào?",
+  },
+  {
+    option: "Câu hỏi số 7",
+    style: {
+      backgroundColor: "#028678",
+      textColor: "white",
+    },
+    question:
+      "Loại cây đặc trưng cho ngày tết, không hoa, không trái mà ma quỷ rất sợ?",
+  },
+  {
+    option: "Câu hỏi số 8",
+    style: {
+      backgroundColor: "#858587",
+      textColor: "white",
+    },
+    question:
+      "Nước nào trên thế giới không có giao thừa: Việt Nam, Hàn Quốc hay Ấn Độ?",
+  },
+];
